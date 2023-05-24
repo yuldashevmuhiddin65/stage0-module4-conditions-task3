c295ae8db76d0ed4cfe36b59a1d5f9547dfd8ec7
m.yuldashev <yuldashevmuhiddin65@gmail.com>
2023-05-24 15:38:03 +0500

    I`ve finishied

--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -1,15 +1,16 @@
 package school.mjc.stage0.conditions.task3;
 
+import java.awt.*;
 import java.util.Scanner;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
-        if (salary < 0) {
+
+        if (salary <= 0){
             System.out.println("wrong input!");
+            return ;
         }
-
         double tax = 0;
-
         if (salary <= 10000) {
             tax = salary * 0.15;
         } else if (salary <= 20000) {
@@ -17,7 +18,8 @@ public class SalaryCalculator {
         } else {
             tax = salary * 0.20;
         }
-
         double netSalary = salary - tax;
+        System.out.println(netSalary);
     }
 }
+
